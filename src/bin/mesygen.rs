ba705db78f9977d356bb003b5cc8745c15ba1dca
Georg Brandl <g.brandl@fz-juelich.de>
2018-06-27 13:57:37 +0200

    fix needing running openocd to work, simplify logging

--- src/bin/mesygen.rs
+++ src/bin/mesygen.rs
@@ -19,7 +19,7 @@ use core::cell::Cell;
 use arrayvec::ArrayVec;
 use byteorder::{ByteOrder, LE};
 use board::{Peripherals, CorePeripherals, SYST};
-use log::{Record, Level, Metadata, LevelFilter};
+use log::{Record, Metadata, LevelFilter};
 use cortex_m::peripheral;
 use cortex_m::interrupt::{self, Mutex};
 use cortex_m_rt::ExceptionFrame;
@@ -35,18 +35,16 @@ const PORT: u16 = 54321;
 
 struct ItmLogger;
 
+fn itm() -> &'static mut peripheral::itm::Stim {
+    unsafe { &mut (*peripheral::ITM::ptr()).stim[0] }
+}
+
 impl log::Log for ItmLogger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Trace
-    }
-
     fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            let stim = unsafe { &mut (*peripheral::ITM::ptr()).stim[0] };
-            iprintln!(stim, "[{}] {}", record.level(), record.args());
-        }
+        iprintln!(itm(), "[{}] {}", record.level(), record.args());
     }
 
+    fn enabled(&self, _: &Metadata) -> bool { true }
     fn flush(&self) {}
 }
 
@@ -56,8 +54,11 @@ static ETH_TIME: Mutex<Cell<i64>> = Mutex::new(Cell::new(0));
 entry!(main);
 
 fn main() -> ! {
-    log::set_logger(&LOGGER).unwrap();
-    log::set_max_level(LevelFilter::Info);
+    // enable logging if someone is listening on ITM
+    if itm().is_fifo_ready() {
+        log::set_logger(&LOGGER).unwrap();
+        log::set_max_level(LevelFilter::Info);
+    }
 
     let p = Peripherals::take().unwrap();
     let mut cp = CorePeripherals::take().unwrap();
@@ -338,7 +339,7 @@ impl Generator {
         }
         let mut nevents = (elapsed / self.interval) as usize;
         if nevents > 220 {
-            info!("too many events for single packet, limiting...");
+            // info!("too many events for single packet, limiting...");
             nevents = 220;
         }
         match sock.send(42 + 6*nevents, self.endpoint) {
